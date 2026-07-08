6e961a9dc5329681208384a6c8e99a4dd4356656
Alex Thomas <48531362+AlexThomas-Graft0@users.noreply.github.com>
2026-07-08 21:02:12 +0100

    add lib/supabaseClient.ts

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createBrowserClient } from "@supabase/ssr";
+
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+  { db: { schema: "nu_rose_tattoos_3367b0" } },
+);
